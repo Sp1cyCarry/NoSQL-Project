8408340cefe380c9aaf432362cd5355f63839578
dziublykz <dziublykz@gmail.com>
2025-10-03 17:45:06 +0200

    OdunayoNik

--- NoSQL-Project/Models/ErrorViewModel.cs
+++ NoSQL-Project/Models/ErrorViewModel.cs
@@ -2,6 +2,16 @@ namespace NoSQL_Project.Models;
 
 public class ErrorViewModel
 {
+
+
+
+
+
+
+
+
+
+
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
