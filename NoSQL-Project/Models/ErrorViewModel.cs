f44db5fd2101608d86c2c6f87abc9ed6ef3d2195
SpicyCarry <736115@student.inholland.nl>
2025-10-03 16:09:40 +0200

    initial commit

---
+++ NoSQL-Project/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NoSQL_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
